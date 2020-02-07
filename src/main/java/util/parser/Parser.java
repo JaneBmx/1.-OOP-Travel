e27726a44f0b48daf5783b1a347568ee92403169
Jane Sonina <janeridebmx@gmail.com>
2020-02-08 00:00:45 +0300

    Remove redundant folders

--- src/main/java/util/parser/Parser.java
+++
@@ -1,9 +0,0 @@
-package util.parser;
-
-import entity.Tour;
-
-import java.util.Set;
-
-public interface Parser {
-    Set<Tour> parse(String text);
-}
--- src/main/java/util/reader/Reader.java
+++
@@ -1,7 +0,0 @@
-package util.reader;
-
-import exception.FileReadException;
-
-public interface Reader {
-    String read(String path) throws FileReadException;
-}
